7b485b0c78d6b0d857d3cd09d8943ce04ba2d7d7
austinchennn <guangchengchen45@gmail.com>
2026-05-21 22:34:57 -0400

    Finish src/main/java/org/translation/LanguageCodeConverter.java & fix:git add src/main/java/org/translation/CountryCodeConverter.java

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +14,10 @@ import java.util.Map;
  */
 public class LanguageCodeConverter {
 
-    // TODO Task: pick appropriate instance variables to store the data necessary for this class
+    private static final int NUM_PART = 2;
+    private final Map<String, String> langToCode;
+    private final Map<String, String> codeToLang;
+
 
     /**
      * Default constructor which will load the language codes from "language-codes.txt"
@@ -25,6 +29,7 @@ public class LanguageCodeConverter {
 
     /**
      * Overloaded constructor which allows us to specify the filename to load the language code data from.
+     *
      * @param filename the name of the file in the resources folder to load the data from
      * @throws RuntimeException if the resource file can't be loaded properly
      */
@@ -33,12 +38,26 @@ public class LanguageCodeConverter {
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
+            langToCode = new HashMap<>();
+            codeToLang = new HashMap<>();
 
-            // TODO Task: use lines to populate the instance variable
-            //           tip: you might find it convenient to create an iterator using lines.iterator()
+            Iterator<String> iterator = lines.iterator();
+            if (iterator.hasNext()) {
+                iterator.next();
+            }
 
-        // TODO Checkstyle: '}' on next line should be alone on a line.
-        } catch (IOException | URISyntaxException ex) {
+            while (iterator.hasNext()) {
+                String line = iterator.next();
+                String[] parts = line.split("\t");
+                if (parts.length >= NUM_PART) {
+                    String language = parts[0];
+                    String code = parts[1];
+                    langToCode.put(language, code);
+                    codeToLang.put(code, language);
+                }
+            }
+        }
+        catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
 
@@ -46,30 +65,30 @@ public class LanguageCodeConverter {
 
     /**
      * Returns the name of the language for the given language code.
+     *
      * @param code the language code
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return code;
+        return codeToLang.get(code);
     }
 
     /**
      * Returns the code of the language for the given language name.
+     *
      * @param language the name of the language
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return language;
+        return langToCode.get(language);
     }
 
     /**
      * Returns how many languages are included in this code converter.
+     *
      * @return how many languages are included in this code converter.
      */
     public int getNumLanguages() {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return 0;
+        return codeToLang.size();
     }
 }
